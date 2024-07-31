d2d6fed9d0edddaaa4df9c7765cc8bf33a85d718
alexeygregor <161445776+alexeygregor@users.noreply.github.com>
2024-07-31 16:01:15 +0600

    Add files via upload

---
+++ player/player.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "player.h"
+using namespace std;
+
+class Track
+{
+  string name;
+  string create;
+  string length;
+
+public:
+
+  void setName(string n);
+
+  string getName();
+
+  void setCreate(string c);
+
+  string getCreate();
+
+  void setLength(string l);
+
+  string getLength();
+};
+
+class Player
+{
+  bool stop_ch = true;
+  bool pause_ch = true;
+  vector <Track> tracks;
+
+public:
+
+  void add(Track &track);
+
+  void play(Track &track, int pos);
+
+  int next(Track &track, int pos);
+
+  void pause();
+
+  void stop();
+
+  int exit(int q);
+};
